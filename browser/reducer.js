7010e022f1abae91095de3815d33230c326edcf8
Jonah Livingston <11jbl4@queensu.ca>
2017-07-29 04:13:54 -0700

    changed gitignore

--- browser/reducer.js
+++ browser/reducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import LoginReducer from './Login/LoginReducer';
 import CreateReducer from './Create/CreateReducer';
 import PreviewReducer from './Preview/PreviewReducer';
 
 const rootReducer = combineReducers({
-  auth: LoginReducer,
   create: CreateReducer,
   preview: PreviewReducer,
 });
